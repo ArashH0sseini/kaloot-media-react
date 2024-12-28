a7303e7b07a0937643807a3f8f58c1c075cc7849
Arash Hosseini <arashhosseini733@gmail.com>
2024-12-28 17:25:42 +0330

    add custom loading to project

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import { Suspense, useState, lazy, useEffect } from 'react'
 import Loading from './components/Loading';
 const LazyComponent = lazy(() => import('./components/LazyComponent'));
 const App = () => {
-  // const [isLoading, setIsLoading] = useState(true);
+  const [minLoadingTime, setMinLoadingTime] = useState(true);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 3000);
-  // }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMinLoadingTime(false);
+    }, 3000); // 3 ثانیه
 
-  return (
-        <Suspense fallback={<Loading />}>
-          <LazyComponent />
-        </Suspense>
-  );
-}
+    return () => clearTimeout(timer); // تمیز کردن تایمر
+  }, []);
+
+  if (minLoadingTime) {
+    return <Loading />;
+  }
+
+  return <Suspense fallback={<Loading />}><LazyComponent /></Suspense>;
+};
 
 export default App;
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,22 +1,9 @@
-import { useRef,useEffect } from "react";
-import loading from "../assets/video/load.webm";
+import loading from "../assets/images/loading.gif";
 
 const Loading = () => {
-    const videoRef = useRef(null);
-
-    useEffect(() => {
-        if (videoRef.current) {
-          videoRef.current.playbackRate = 3;
-        }
-      }, []);
-      
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-[#111b1b] z-50">
-      <video ref={videoRef} autoPlay muted
-        className="w-full h-auto"
-      >
-        <source src={loading} type="video/mp4" />
-      </video>
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black z-50">
+      <img src={loading} alt="loading" />
     </div>
   );
 };
