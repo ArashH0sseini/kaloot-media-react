078827b5b4aa0eba9877c393efc4f3dea622cbe7
Arash Hosseini <arashhosseini733@gmail.com>
2025-01-11 21:15:06 +0330

    Scroll to top on route change using ScrollToTop component

--- src/helpers/Pages.js
+++ src/helpers/Pages.js
@@ -6,18 +6,23 @@ import VFXPage from "../components/pages/VFXPage";
 import CampaignPage from "../components/pages/CampaignPage";
 import VideoPage from "../components/pages/VideoPage";
 import MotionGraphicsPage from "../components/pages/MotionGraphicsPage";
+import ScrollToTop from "./ScrollToTop";
 
 const Pages = () => {
+
   return (
-    <Routes>
-      <Route path="/" element={<App />} >
-        <Route path="/" element={<Home />} />
-        <Route path="/campaigns" element={<CampaignPage />} />
-        <Route path="/vfx" element={<VFXPage />} />
-        <Route path="/Video" element={<VideoPage />} />
-        <Route path="/motion" element={<MotionGraphicsPage />} />
-      </Route>
-    </Routes>
+    <div>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<App />} >
+          <Route path="/" element={<Home />} />
+          <Route path="/campaigns" element={<CampaignPage />} />
+          <Route path="/vfx" element={<VFXPage />} />
+          <Route path="/Video" element={<VideoPage />} />
+          <Route path="/motion" element={<MotionGraphicsPage />} />
+        </Route>
+      </Routes>
+    </div>
   );
 };
 
